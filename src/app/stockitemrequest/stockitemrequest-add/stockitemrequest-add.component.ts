24739d6adf99f4d2b41aabaa0e8b1208f307c29e
tendaikawadza <tendaikawadza49@gmail.com>
2023-12-31 16:34:53 +0200

    first commit

---
+++ src/app/stockitemrequest/stockitemrequest-add/stockitemrequest-add.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-stockitemrequest-add',
+  templateUrl: './stockitemrequest-add.component.html',
+  styleUrls: ['./stockitemrequest-add.component.css']
+})
+export class StockitemrequestAddComponent {
+gridRows: any;
+clearSignature() {
+throw new Error('Method not implemented.');
+}
+onFileSelected($event: Event) {
+throw new Error('Method not implemented.');
+}
+addRow() {
+throw new Error('Method not implemented.');
+}
+removeRow(_t56: number) {
+throw new Error('Method not implemented.');
+}
+myForm: FormGroup<any>;
+newstockitemrequest() {
+throw new Error('Method not implemented.');
+}
+
+}
